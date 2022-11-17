cadc0c730cf0d43643ec27ff06fdbc65fef30d2d
ramirochangomoreno <rama.exe.13@gmail.com>
2022-11-17 14:41:57 -0300

    teminando hook

--- src/componentes/Canvas.jsx
+++ src/componentes/Canvas.jsx
@@ -1,55 +1,47 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { useOnDraw } from "../hook/useOnDraw";
 
+export default function Canvas({ width, height, className, setupCanvas }) {
+  const [mousseEvent, setMousseEvent] = useState(false);
 
-export default function Canvas({
-  width,
-  height,
-  className,
-}) {
+  const { onCanvasMouseDown, setCanvasRef } = useOnDraw(onDraw);
 
-  const [mousseEvent,setMousseEvent]=useState(false)
-
-const {
-  onCanvasMouseDown
-  ,setCanvasRef}=useOnDraw(onDraw)
-
-
-
-
-function dibujarLinea(start,end,ctx,color,width){
-start=start??end
+  function dibujarLinea(start, end, ctx, color, width) {
+    start = start ?? end;
     ctx.beginPath();
-    ctx.style=color
-    ctx.lineWidth=width;
-    ctx.strokeStyle=color
-    ctx.moveTo(start.x,start.y)
-    ctx.lineTo(end.x,end.y)
-    ctx.stroke()
+    ctx.style = color;
+    ctx.lineWidth = width || 10;
+    ctx.strokeStyle = color;
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke();
 
-    ctx.fillStyle=color;
-    ctx.beginPath()
-  ctx.arc(start.x,start.y,width,0,2*Math.PI)
-  ctx.fill()
-}
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(start.x, start.y, width, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 
-const imgCrs=()=>{
-  
-}
+  const imgCrs = () => {};
 
-function onDraw(ctx,point,prevPoint){
-  dibujarLinea(prevPoint,point,ctx,'#00ddcc',15)
-}
+  function onDraw(ctx, point, prevPoint) {
+    dibujarLinea(
+      prevPoint,
+      point,
+      ctx,
+      setupCanvas?.colorTrazo,
+      setupCanvas?.rangeTrazo
+    );
+  }
 
   return (
     <canvas
-    onMouseDown={onCanvasMouseDown}
-    onMouseUp={()=>{setMousseEvent(false)}}
-    ref={setCanvasRef}
-    className={className}
-
+      onMouseDown={onCanvasMouseDown}
+      ref={setCanvasRef}
+      className={className}
       width={width}
       height={height}
- />
+    />
   );
 }
